cd257c1a10ef96a4aa58fb5202fc3550bf1c0811
seifrajhi <rajhiseif@gmail.com>
2024-10-13 12:15:18 +0200

    :gem: Revamp: Modernize site with new features, fresh blogs, and enhanced UI/UX

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2488ea56f400e0738f06.js"
+    "url": "webpack-runtime-a811e3d941414a983041.js"
   },
   {
     "url": "framework-c989293af61a95023f7f.js"
   },
   {
-    "url": "app-9a8612690ac49bf4d185.js"
+    "url": "app-2c6c6643a2a82059cb23.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "de5449cb593d9b5e22d6670089991d9e"
+    "revision": "472d35362c632449d03fb2e5b5c3cebb"
   },
   {
     "url": "static/webfonts/s/dancingscript/v25/If2cXTr6YS-zF4S-kcSWSVi_sxjsohD9F50Ruu7BMSo3Sup8.woff2"
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-9a8612690ac49bf4d185.js`))) {
+  if (!resources || !(await caches.match(`/app-2c6c6643a2a82059cb23.js`))) {
     return await fetch(event.request)
   }
 
