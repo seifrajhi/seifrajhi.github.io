5c233537e207cde7d20e5de601d16f6509a67d77
seifrajhi <rajhiseif@gmail.com>
2024-10-16 22:31:20 +0200

    :gem: Revamp: Modernize site with new features, fresh blogs, and enhanced UI/UX

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b52987cbf9e0728ddd5e.js"
+    "url": "webpack-runtime-9c85b546bff97d6f7212.js"
   },
   {
     "url": "framework-c989293af61a95023f7f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d097a8dbc9ebd8ba488fdd157a045ddd"
+    "revision": "cd9325f3f600b7d58790d4669eb11303"
   },
   {
     "url": "static/webfonts/s/dancingscript/v25/If2cXTr6YS-zF4S-kcSWSVi_sxjsohD9F50Ruu7BMSo3Sup8.woff2"
