69d3cec12daa62f2bf6d65819efdb1ab8632ff8c
seifrajhi <rajhiseif@gmail.com>
2024-11-02 22:52:37 +0100

    :artificial_satellite: new items on k8s, AWS EKS, and software principles 🍉

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0fee783519a33042df8b.js"
+    "url": "webpack-runtime-9bd77e767c2b556866fd.js"
   },
   {
     "url": "framework-c989293af61a95023f7f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "617c9e55903352d1a6d54f6462d0bae5"
+    "revision": "d8e95f21db75007fba5eaaac765cd4fb"
   },
   {
     "url": "static/webfonts/s/dancingscript/v25/If2cXTr6YS-zF4S-kcSWSVi_sxjsohD9F50Ruu7BMSo3Sup8.woff2"
