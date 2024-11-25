373963dd3c013076613c5164bdd127df3d1013e8
saifeddine Rajhi <rajhiseif@gmail.com>
2024-11-25 21:54:03 +0000

    :space_invader: Added new blog post on istio 1.24 🍉

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-414dfe370faa8e694f9b.js"
+    "url": "webpack-runtime-80ade5d9bbec0331f109.js"
   },
   {
     "url": "framework-c989293af61a95023f7f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c5c9c7da7251e54f5f51ae6ca8b0bf94"
+    "revision": "8a46d4a23d8ff3e477fc65c84f21f6f3"
   },
   {
     "url": "static/webfonts/s/dancingscript/v25/If2cXTr6YS-zF4S-kcSWSVi_sxjsohD9F50Ruu7BMSo3Sup8.woff2"
